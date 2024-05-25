92014ec936550921cdfb47b7ce69656af852429b
glebvyblov <283687@niuitmo.ru>
2024-05-25 22:23:52 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -3,4 +3,26 @@
 
 struct TEvents {
     // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    enum TEvent {
+        WriteEvent = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        JobEndEvent,
+    };
+
+    static_assert(TEvent::JobEndEvent < EventSpaceEnd(NActors::TEvents::ES_PRIVATE), "EventSpace size too small");
+
+    struct TEventWrite : NActors::TEventLocal<TEventWrite, TEvent::WriteEvent> {
+    public:
+        explicit TEventWrite(int64_t value) : value_(value) {
+        }
+
+        [[nodiscard]] int64_t GetValue() const noexcept {
+            return value_;
+        }
+
+    private:
+        const int64_t value_;
+    };
+
+    struct TEventJobEnd : NActors::TEventLocal<TEventJobEnd, TEvent::JobEndEvent> {
+    };
 };
